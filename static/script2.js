9568aafd9ae758e214c2bf3630c4797290b8cdf6
anurag5ahay <anuragtbg@gmail.com>
2023-01-16 21:13:39 +0530

    Location fix

--- static/script2.js
+++ static/script2.js
@@ -4,16 +4,11 @@ let lat = document.getElementById("lat");
 
 cur_loc_btn.addEventListener("click", () => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        lon.value = pos.coords.longitude;
-        lat.value = pos.coords.latitude;
-        cur_loc_btn.parentNode.submit();
-      },
-      (err) => {
-        window.alert("Please Allow Location Access");
-      }
-    );
+    navigator.geolocation.getCurrentPosition((pos) => {
+      lon.value = pos.coords.longitude;
+      lat.value = pos.coords.latitude;
+      cur_loc_btn.parentNode.submit();
+    });
   } else {
     window.alert("Please Enable Location Access");
   }
